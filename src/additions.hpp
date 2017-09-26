143c266e0ef89fa3bcfac98e68fe62d8bdd047a2
Hydroque <deocojo1@gmail.com>
2017-09-26 02:28:17 -0400

    Added additions hpp file for maybe converting this to a C++ application, too, or stripping the main to embed the console.

--- src/additions.hpp
+++ src/additions.hpp
@@ -2,11 +2,19 @@
 #ifndef ADDITIONS_H_
 #define ADDITIONS_H_
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 // I don't like this include here
-#include "lua.h"
+#include "lua.h" 
 
 int stack_dump(lua_State* L);
 
 void additions_add(lua_State* L);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
