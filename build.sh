d0a0fdf28c2502b6b0190f275409249ed2817d51
Hydroque <deocojo1@gmail.com>
2017-12-30 23:07:12 -1000

    gcc needs to link -Wl,-E and -ldl

--- build.sh
+++ build.sh
@@ -10,19 +10,19 @@ cd src
 gcc -std=gnu99 -Wall -O2 -g0 -DLUACON_ADDITIONS -c console.c consolew.c additions.c
 
 # Link luaw
-gcc -std=gnu99 -s -Wall -O2 -g0 -o lua_add console.o additions.o -lm -llua
+gcc -std=gnu99 -s -Wall -O2 -g0 -o lua_add console.o additions.o -Wl,-E -ldl  -lm -llua
 
 # Link lua
-gcc -std=gnu99 -s -Wall -O2 -g0 -o luaw_add consolew.o additions.o -lm -llua
+gcc -std=gnu99 -s -Wall -O2 -g0 -o luaw_add consolew.o additions.o -Wl,-E -ldl -lm -llua
 
 # Compile everything release w/o additions
 gcc -std=gnu99 -Wall -O2 -g0 -c console.c consolew.c
 
 # Link luaw
-gcc -std=gnu99 -s -Wall -O2 -g0 -o lua console.o -lm -llua
+gcc -std=gnu99 -s -Wall -O2 -g0 -o lua console.o -Wl,-E -ldl -lm -llua
 
 # Link lua
-gcc -std=gnu99 -s -Wall -O2 -g0 -o luaw consolew.o -lm -llua
+gcc -std=gnu99 -s -Wall -O2 -g0 -o luaw consolew.o -Wl,-E -ldl -lm -llua
 
 if [ -d $root ]; then
 	rm -r --one-file-system -r -d $root
