0b060e913964269b2d60d8a0ad4163e9c7c5ac8c
Hydroque <deocojo1@gmail.com>
2017-12-30 22:49:34 -1000

    added build.sh for linux

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+root=../bin/debug
+objdir=../obj
+resdir=../res
+
+cd src
+
+# Compile everything release w/ additions
+gcc -std=gnu99 -Wall -O2 -g0 -DLUACON_ADDITIONS -c console.c consolew.c additions.c
+
+# Link luaw
+gcc -std=gnu99 -s -Wall -O2 -g0 -o lua_add console.o additions.o -lm -llua
+
+# Link lua
+gcc -std=gnu99 -s -Wall -O2 -g0 -o luaw_add consolew.o additions.o -lm -llua
+
+# Compile everything release w/o additions
+gcc -std=gnu99 -Wall -O2 -g0 -c console.c consolew.c
+
+# Link luaw
+gcc -std=gnu99 -s -Wall -O2 -g0 -o lua console.o -lm -llua
+
+# Link lua
+gcc -std=gnu99 -s -Wall -O2 -g0 -o luaw consolew.o -lm -llua
+
+if [ -d $root ]; then
+	rm -r --one-file-system -r -d $root
+fi
+mkdir -p $root
+mkdir -p $root/res
+
+mv *.o $objdir
+mv lua $root
+mv luaw $root
+mv lua_add $root
+mv luaw_add $root
+cp -r $resdir/* $root/res
+
+cd $root
+
+strip --strip-all lua_add
+strip --strip-all luaw_add
+strip --strip-all lua
+strip --strip-all luaw
+
