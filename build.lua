cdd40ac52fa877e056d969fa57ba93d196c6d765
Hydroque <deocojo1@gmail.com>
2018-01-14 03:06:41 -0500

    simplified and better support for multiple lua versions, included jit

--- build.lua
+++ build.lua
@@ -10,6 +10,10 @@ require("luaadd")
 require("build_func")
 
 
+local lua_ver = "lua51"
+local lua_define = " -DLUA_JIT_51"
+
+
 --[[
 	gcc setup
 	
@@ -23,7 +27,7 @@ gcc = {
 	extra_warnings = "";
 	include_dir = " -I. -Iinclude";
 	library_dir = " -L. -Llib -Lobj -Ldll";
-	ccextras = " -s";
+	ccextras = " -s" .. lua_define;
 	ldextras = choose_opt(" -s", " -s -fPIC -Wl,-E", " -s -Wl,-E");
 	defines = choose_opt(" -D__USE_MINGW_ANSI_STDIO=1", "", "");
 }
@@ -33,28 +37,37 @@ gcc.O = gcc.debug and 0 or 2
 install_path = plat == os.types.Windows and (gcc.debug and ("bin\\Debug") or ("bin\\Release")) or ((plat == os.types.Linux or plat == os.types.MacOSX) and (gcc.debug and ("bin/Debug") or ("bin/Release")))
 
 
+targ_dir = (gcc.debug and "DEBUG" or "RELEASE")
+
 
 --[[
 	cache setup
 	
 --]]
 
-local targ_dir = (gcc.debug and "DEBUG" or "RELEASE")
+local targ1_exe = {enc_exe("luaw")}
+local targ1_compile_units = {"consolew.c", "darr.c"}
 
-local targ_exe = {enc_exe("luaw")}
-local targ_dll = {enc_dll("luaadd")}
-local targ_units1 = {"consolew.c", "darr.c"}
-local targ_units2 = {"additions.c"}
+local targ2_dll = {enc_dll("luaadd")}
+local targ2_compile_units = {"additions.c"}
 
-local targ_compile_units1 = check_cache(targ_units1, "src/", "obj/", "o")
-local targ_compile_units2 = check_cache(targ_units2, "src/", "obj/", "o")
+local exe_test = true
 
-local exe_test = 
-	   check_bin_cache(targ_exe, "src/", install_path .. "\\", targ_units1)
-	or check_bin_cache(targ_exe, "src/", install_path .. "\\", targ_units2)
-	or check_bin_cache(targ_dll, "src/", install_path .. "\\", targ_units1)
-	or check_bin_cache(targ_dll, "src/", install_path .. "\\", targ_units2)
+if(cache == nil)then
+	targ1_compile_units = check_cache(targ1_compile_units, "src/", "obj/", "o")
+	targ2_compile_units = check_cache(targ2_compile_units, "src/", "obj/", "o")
 
+	exe_test = 
+		   check_bin_cache(targ1_exe, "src/", install_path .. "\\", targ1_compile_units)
+		or check_bin_cache(targ1_exe, "src/", install_path .. "\\", targ2_compile_units)
+		or check_bin_cache(targ2_dll, "src/", install_path .. "\\", targ1_compile_units)
+		or check_bin_cache(targ2_dll, "src/", install_path .. "\\", targ2_compile_units)
+end
+
+if(#targ1_compile_units == 0 and #targ2_compile_units == 0 and not exe_test and force == nil)then
+	print("Done. Up to date.")
+	return
+end
 
 
 
@@ -63,18 +76,18 @@ local exe_test =
 
 --]]
 
-local std_libs = enc_dll(" -llua53") .. choose_opt("", " -lm -ldl", " -lm -ldl")
+local std_libs = enc_dll(" -l" .. lua_ver) .. choose_opt("", " -lm -ldl", " -lm -ldl")
 
 
-local targ_luaadd_dll_a = nil
-local targ_luaadd_dll = enc_dll("luaadd")
-local targ_luaadd_o = {"additions.o"}
-local targ_luaadd_libs = std_libs
+local targ1_luaw_exe = enc_exe("luaw")
+local targ1_luaw_exe_o = {"consolew.o", "darr.o"}
+local targ1_lua_exe_libs = std_libs
 
 
-local targ_luaw_exe = enc_exe("luaw")
-local targ_luaw_exe_o = {"consolew.o", "darr.o"}
-local targ_lua_exe_libs = std_libs
+local targ2_luaadd_dll_a = nil
+local targ2_luaadd_dll = enc_dll("luaadd")
+local targ2_luaadd_o = {"additions.o"}
+local targ2_luaadd_libs = std_libs
 
 
 
@@ -85,56 +98,44 @@ local targ_lua_exe_libs = std_libs
 
 -- compile string
 local compile_str1 = gcc_c(
-		gcc.g,
-		gcc.O,
-		gcc.warnings,
-		gcc.extra_warnings,
+		gcc.g, gcc.O, gcc.warnings, gcc.extra_warnings,
 		gcc.defines,
 		gcc.ccextras,
 		gcc.include_dir,
 		gcc.library_dir,
-		targ_compile_units1)
+		targ1_compile_units)
 		
 local compile_str2 = gcc_c(
-		gcc.g,
-		gcc.O,
-		gcc.warnings,
-		gcc.extra_warnings,
-		gcc.defines .. " -DLUACON_ADDITIONS ",
+		gcc.g, gcc.O, gcc.warnings, gcc.extra_warnings,
+		gcc.defines,
 		gcc.ccextras,
 		gcc.include_dir,
 		gcc.library_dir,
-		targ_compile_units2)
+		targ2_compile_units)
 		
 -- generate dll/so string
 local linker_str_dll1 = choose_gcc_dll()(
-		targ_luaadd_dll_a,
-		gcc.g,
-		gcc.O,
-		gcc.warnings,
-		gcc.extra_warnings,
+		targ2_luaadd_dll_a,
+		gcc.g, gcc.O, gcc.warnings, gcc.extra_warnings,
 		gcc.defines,
 		gcc.ldextras,
 		gcc.include_dir,
 		gcc.library_dir,
-		targ_luaadd_dll,
-		targ_luaadd_o,
-		targ_luaadd_libs)
+		targ2_luaadd_dll,
+		targ2_luaadd_o,
+		targ2_luaadd_libs)
 
 
 -- luaw linker string
 local linker_str_exe1 = gcc_l(
 		gcc.windows,
-		gcc.g,
-		gcc.O,
-		gcc.warnings,
-		gcc.extra_warnings,
+		gcc.g, gcc.O, gcc.warnings, gcc.extra_warnings,
 		gcc.ldextras,
 		gcc.include_dir,
 		gcc.library_dir,
-		targ_luaw_exe,
-		targ_luaw_exe_o,
-		targ_lua_exe_libs)
+		targ1_luaw_exe,
+		targ1_luaw_exe_o,
+		targ1_lua_exe_libs)
 
 
 
@@ -144,17 +145,17 @@ local linker_str_exe1 = gcc_l(
 --]]
 
 -- compile
-if(#targ_compile_units1 > 0 or exe_test or force)then
+if(#targ1_compile_units > 0 or exe_test or force)then
 	compiler_exec(targ_dir, compile_str1)
 	obj_transfer()
 end
-if(#targ_compile_units2 > 0 or exe_test or force)then
+if(#targ2_compile_units > 0 or exe_test or force)then
 	compiler_exec(targ_dir, compile_str2)
 	obj_transfer()
 end
 
 
-if(#targ_compile_units1 + #targ_compile_units2 > 0 or exe_test or force)then
+if(#targ1_compile_units + #targ2_compile_units > 0 or exe_test or force)then
 	-- link dll
 	linker_exec(enc_dll(" luaadd"), linker_str_dll1)
 	obj_transfer()
