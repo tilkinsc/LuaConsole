47f2fd45eac05e56e5083d79166ef980e010ba63
Hydroque <deocojo1@gmail.com>
2018-02-02 03:04:52 -0500

    updated copyright, now cleans up profiling files

--- clean.sh
+++ clean.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+# MIT License
+# 
+# Copyright (c) 2017-2018 Cody Tilkins
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 echo '>>> Cleaning bin'
 echo '> bin\Debug'
 rm --one-file-system -f -r -d bin\Debug
@@ -23,4 +45,12 @@ echo '> *.dll'
 rm --one-file-system *.dll
 rm --one-file-system obj\*.dll
 rm --one-file-system src\*.dll
+
+echo '>>> Cleaning Debug'
+echo '> *.gcov'
+rm --one-file-system *.gcov
+echo '> *.gcda'
+rm --one-file-system *.gcda
+echo '> *.gcno'
+rm --one-file-system *.gcno
 
