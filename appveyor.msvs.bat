8859c1da8b19596b931197d20b1872acc7dc71c0
Cody Tilkins <ctilkins@mail.com>
2021-03-01 17:35:23 -0500

    Update appveyor.msvs.bat

--- appveyor.msvs.bat
+++ appveyor.msvs.bat
@@ -2,7 +2,7 @@
 
 setlocal
 	
-	"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 	
 	prereqs.bat download
 	build.msvs.bat driver luajit
