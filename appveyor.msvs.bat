bec012ba83cc5861f4f5a0b8e6067a87682bc2b0
Cody Tilkins <ctilkins@mail.com>
2021-03-01 17:44:16 -0500

    Update appveyor.msvs.bat

--- appveyor.msvs.bat
+++ appveyor.msvs.bat
@@ -1,3 +1,25 @@
+@REM MIT License
+@REM 
+@REM Copyright (c) 2017-2021 Cody Tilkins
+@REM 
+@REM Permission is hereby granted, free of charge, to any person obtaining a copy
+@REM of this software and associated documentation files (the "Software"), to deal
+@REM in the Software without restriction, including without limitation the rights
+@REM to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+@REM copies of the Software, and to permit persons to whom the Software is
+@REM furnished to do so, subject to the following conditions:
+@REM 
+@REM The above copyright notice and this permission notice shall be included in all
+@REM copies or substantial portions of the Software.
+@REM 
+@REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+@REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+@REM FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+@REM AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+@REM LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+@REM OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+@REM SOFTWARE.
+
 @echo off
 
 setlocal
@@ -5,10 +27,15 @@ setlocal
 	call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 	
 	prereqs.bat download
+	
 	build.msvs.bat driver luajit
+	
 	build.msvs.bat package lua-5.4.2
+	
 	build.msvs.bat package lua-5.3.6
+	
 	build.msvs.bat package lua-5.2.4
+	
 	build.msvs.bat package lua-5.1.5
 	
 endlocal
