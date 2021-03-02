f891416d3b3e86e3a505940642b59ffd53243125
Polite Kiwi <deocojo1@gmail.com>
2021-03-02 00:01:35 -0500

    Inited github ci build/testing system

---
+++ .github/workflows/ci.linux.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# MIT License
+# 
+# Copyright (c) 2017-2021 Cody Tilkins
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -e
+
+
+# Init
+printf "> PREREQS\n"
+if [[ ! -d "lua-all" || ! -d "luajit-2.0" ]]; then
+	printf "Not cached. Downloading...\n"
+	
+	./prereqs.sh download
+	
+	printf "Github cache created.\n"
+else
+	printf "Cached.\n"
+fi
+
+
+# Building
+printf "> BUILDING\n"
+export debug=1
+export debug_coverage=1
+./build.linux.sh driver luajit
+./build.linux.sh package lua-5.4.2
+./build.linux.sh package lua-5.3.6
+./build.linux.sh package lua-5.2.4
+./build.linux.sh package lua-5.1.5
+
+
+# Testing
+printf "> TESTING\n"
+export LD_LIBRARY_PATH=".:${LD_LIBRARY_PATH}"
+printf "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}\n"
+pushd bin/Debug
+	printf "Test 1\n"
+	./luaw -e "print('Everything went okay')"
+popd
+
+
+printf "Testing complete.\n"
+exit 0
+
