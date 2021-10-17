50b13263dcbe534f9fff16ddf5a305c45b0aa2d4
tilkinsc <ctilkins@mail.com>
2021-10-16 22:35:03 -0400

    Forgot one call

--- appveyor.msvs.bat
+++ appveyor.msvs.bat
@@ -24,7 +24,7 @@
 
 setlocal
 	
-	"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 	
 	REM Init
 	echo ^> PREREQS
